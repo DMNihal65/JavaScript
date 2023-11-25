ba85763aa8beebd8902cddd6a4622ce8f0ea769d
DMNihal65 <Nihaldm65@gmail.com>
2023-11-25 19:56:15 +0530

    javascript datatypes 01

---
+++ Ass/JavascriptCourse/Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Treat js code as newer version
+
+// alert("hello world") //We use node js not the browser
+
+console.log(3+3); console.log('Nihal') //Not Readable and good Practice
+
+//Good practice
+console.log(3+3); 
+console.log('Nihal') 
+
+let name = "Nihal"
+let age =18
+let Loggedin = false
+let state;
+let home = null; //empty
+
+
+//Number => 2 to power of 2
+//bigint
+//string => "nihal" 
+//boolean => true,false
+//null => Standalone value
+//undefined => not assigned
+//symbol => Uniqueness
+
+
+//typeof
+console.log(typeof "nihal") //string
+console.log(typeof undefined) //undefuned
+console.log(typeof null)// object
---
+++ Ass/JavascriptCourse/Day1.js
@@ -0,0 +1,26 @@
+//Camale case - myNameIsNihal
+const accountId = 13334
+let accountEmail = "Nihal@gmail.com"
+
+var accountPassword = "1234" //Depreciated not in use- Becase of the use in blockscope
+
+accountCity = "Karnataka" //can be declared but not a good practice
+
+// accountId = 2
+//We cannot change the const variable
+
+//undefined
+let accountname ;
+
+
+
+accountEmail= "nothing@email.com"
+accountPassword = "12345"
+accountCity = "bangalore"
+
+console.log(accountId)
+
+//Table structure logging
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountname])
+
